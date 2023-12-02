cca88fb85029490eee238fea14ad8f5834543bb2
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-12-01 23:31:46 -0600

    02: Cleaning up.

--- 02/a.rb
+++ 02/a.rb
@@ -1,44 +1,23 @@
 #!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
 file = ARGV[0] || 'input.txt'
 #file = ARGV[0] || 'example.txt'
 
 sum = 0
-File.readlines(file).each { |line|
-  games = line.sub(/Game (\d+): +/, '')
-  id = $1
-  good = true
-  games.split(';').each { |game| 
-    puts
-    p game
-    green = /(\d+) green/.match(game)
-    if green then
-      if green[1].to_i > 13 then
-        good = false
-      end
-    end
-
-    green = /(\d+) blue/.match(game)
-    if green then
-      if green[1].to_i > 14 then
-        good = false
-      end
-    end
-
-    green = /(\d+) red/.match(game)
-    if green then
-      if green[1].to_i > 12 then
-        good = false
-      end
-    end
-
-  }
-  if good then
-    sum += id.to_i
-    puts "GOOD: #{id}"
-  end
-}
+File.readlines(file).each do |line|
+	games = line.sub(/Game (\d+): +/, '')
+	id = $1
+	good = true
+	games.split(';').each do |game|
+		p game
+		m = /(\d+) red/.match(game)
+		good = false if m and m[1].to_i > 12
+		m = /(\d+) green/.match(game)
+		good = false if m and m[1].to_i > 13
+		m = /(\d+) blue/.match(game)
+		good = false if m and m[1].to_i > 14
+	end
+	sum += id.to_i if good
+end
 p sum
-
-#p File.readlines(file).map { |line| (line[/\d/] + line.reverse[/\d/]).to_i }.inject(:+)
-
 
--- 02/b.rb
+++ 02/b.rb
@@ -1,48 +1,36 @@
 #!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
 file = ARGV[0] || 'input.txt'
 #file = ARGV[0] || 'example.txt'
 
 sum = 0
-File.readlines(file).each { |line|
-  games = line.sub(/Game (\d+): +/, '')
-  id = $1
-  good = true
+File.readlines(file).each do |line|
+	games = line.sub(/Game (\d+): +/, '')
+	id = $1
+	good = true
 
-  red = 0
-  green = 0
-  blue = 0
-  power = 0
+	red = 0
+	green = 0
+	blue = 0
+	power = 0
 
-  games.split(';').each { |game| 
-    puts
-    p game
+	games.split(';').each do |game|
+		puts
+		p game
 
-    m = /(\d+) green/.match(game)
-    if m then
-      if m[1].to_i > green then
-        green = m[1].to_i
-      end
-    end
+		m = /(\d+) green/.match(game)
+		green = m[1].to_i if m and m[1].to_i > green
 
-    m = /(\d+) blue/.match(game)
-    if m then
-      if m[1].to_i > blue then
-        blue = m[1].to_i
-      end
-    end
+		m = /(\d+) blue/.match(game)
+		blue = m[1].to_i if m and m[1].to_i > blue
 
-    m = /(\d+) red/.match(game)
-    if m then
-      if m[1].to_i > red then
-        red = m[1].to_i
-      end
-    end
+		m = /(\d+) red/.match(game)
+		red = m[1].to_i if m and m[1].to_i > red
 
-    power = red * blue * green
-    p power
-
-  }
-  sum += power
-}
+		power = red * blue * green
+		p power
+	end
+	sum += power
+end
 p sum
 
