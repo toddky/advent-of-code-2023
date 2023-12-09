800ce94ae95919c02648402f66064bcca5c6d548
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-12-08 22:46:51 -0600

    02: Cleaning up.

--- 02/a.rb
+++ 02/a.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 # vim: ft=ruby noet ts=4 sw=0 sts
+require_relative '../todd'
+
 file = ARGV[0] || 'input.txt'
 #file = ARGV[0] || 'example.txt'
 
@@ -9,15 +11,13 @@ File.readlines(file).each do |line|
 	id = $1
 	good = true
 	games.split(';').each do |game|
-		p game
-		m = /(\d+) red/.match(game)
-		good = false if m and m[1].to_i > 12
-		m = /(\d+) green/.match(game)
-		good = false if m and m[1].to_i > 13
-		m = /(\d+) blue/.match(game)
-		good = false if m and m[1].to_i > 14
+		good = false if [0, game.m1(/(\d+) red/)].max > 12
+		good = false if [0, game.m1(/(\d+) green/)].max > 13
+		good = false if [0, game.m1(/(\d+) blue/)].max > 14
 	end
 	sum += id.to_i if good
 end
-p sum
+
+puts sum.s.bold.yellow
+puts 2679.s.bold.green
 
--- 02/b.rb
+++ 02/b.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 # vim: ft=ruby noet ts=4 sw=0 sts
+require_relative '../todd'
+
 file = ARGV[0] || 'input.txt'
 #file = ARGV[0] || 'example.txt'
 
 sum = 0
 File.readlines(file).each do |line|
 	games = line.sub(/Game (\d+): +/, '')
-	id = $1
-	good = true
 
 	red = 0
 	green = 0
@@ -15,22 +15,14 @@ File.readlines(file).each do |line|
 	power = 0
 
 	games.split(';').each do |game|
-		puts
-		p game
-
-		m = /(\d+) green/.match(game)
-		green = m[1].to_i if m and m[1].to_i > green
-
-		m = /(\d+) blue/.match(game)
-		blue = m[1].to_i if m and m[1].to_i > blue
-
-		m = /(\d+) red/.match(game)
-		red = m[1].to_i if m and m[1].to_i > red
-
-		power = red * blue * green
-		p power
+		red = [red, game.m1(/(\d+) red/)].max
+		blue = [blue, game.m1(/(\d+) blue/)].max
+		green = [green, game.m1(/(\d+) green/)].max
+		power = red * green * blue
+		#p 'game', game, power
 	end
 	sum += power
 end
-p sum
+puts sum.s.bold.yellow
+puts 77607.s.bold.green
 
