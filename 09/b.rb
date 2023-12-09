30a4ec6fcbad552d7bbadae24f838e83d16ce086
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-12-08 23:53:01 -0600

    09: Solved!

---
+++ 09/b.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
+require_relative '../todd'
+
+file = ARGV[0] || 'input.txt'
+#file = ARGV[0] || 'example.txt'
+numbers = file.readlines.map(&:numbers)
+
+def ext(n)
+	diff = []
+	prev = n[0]
+	n[1..-1].each do |i|
+		diff.append(i - prev)
+		prev = i
+	end
+	return n[0] if diff.all? 0
+	return n[0] - ext(diff)
+end
+
+ans = numbers.map {|n|ext(n)}.sum
+
+puts ans.s.bold.yellow
+puts 1091.s.bold.green
+
