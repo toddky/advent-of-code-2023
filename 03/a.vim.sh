2ea69bc11790524b163a4842b8472c1fb07ee7e0
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-01-10 09:46:10 -0600

    03/a.vim.sh: Initial commit.

---
+++ 03/a.vim.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# TODO: https://www.youtube.com/watch?v=QE5c_bWyJ-k
+
+input='example.txt'
+rm -f answer.txt
+
+cat > a.vim <<-EOF
+yyPVr.
+GyypVr.
+ggGI.
+gg$GA.
+:w answer.txt
+:q!
+EOF
+
+echo '================================================================================'
+cat a.vim
+echo '================================================================================'
+
+nvim -u NONE -s a.vim "$input"
+cat answer.txt
+rm -f a.vim answer.txt
+
