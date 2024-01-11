83eb5037015d70e4a039ce3c2dbd21dd86e257b2
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-01-11 13:11:24 -0600

    03/a.vim.sh: Done!

--- 03/a.vim.sh
+++ 03/a.vim.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 # TODO: https://www.youtube.com/watch?v=QE5c_bWyJ-k
 
+input='input.txt'
 input='example.txt'
-rm -f answer.txt
+rm -f a.vim answer.txt
 
-cat > a.vim <<-EOF
+cat > a.vim <<EOF
+:set nowrapscan
 yyPVr.
 GyypVr.
 ggGI.
 gg$GA.
+ggO
+ggma/\d\+
+qq\`anmakhjlejlyggOggPJxJx@qq
+@q
+Gdap
+
+:v/[^\.0-9]/d
+:%s/[^0-9]//g
+:%s/\n/+/
+\$x
+S="
+
 :w answer.txt
 :q!
 EOF
